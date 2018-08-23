36d7a06ba19ffdff0ef6306fa5f22012599137f5
Alex Sh <idfhayal@hotmail.com>
2018-08-23 11:56:33 +0300

    Working version
    paritions print and input

--- yocto_altera_sdcard.sh
+++ yocto_altera_sdcard.sh
@@ -1,4 +1,4 @@
-#!/bi/sh - run in shell
+#!/bi/bash
 #
 # Creating SD card for Altera Cyclone5 Soc
 # Usage: source yocto_altera_install
@@ -6,14 +6,29 @@
 yellow='\E[1;33m'
 NC='\033[0m'
 
+
+if [ $# -eq 0 ]
+then
+  lsblk
+  echo -e "${yellow}Please provide a partition:${NC}"
+  read prt
+else
+  prt=${1}
+fi
+
 echo -e "${yellow}Creating Yocto Rocko SD card for Altera-SoC...${NC}"
-cd ~/poky/build
-rm *sdimage-cyclone5*
+if [ -f ../*sdimage-cyclone5* ]
+then
+  rm ../*sdimage-cyclone5*
+fi
+
+cd ..
 wic create sdimage-cyclone5-arria5 -e core-image-minimal
-umount /dev/sdb1
-umount /dev/sdb2
+umount /dev/${prt}1
+umount /dev/${prt}2
 mv sdimage-cyclone5-*.direct sdimage-cyclone5.direct
-sudo dd if=./sdimage-cyclone5.direct of=/dev/sdb
-umount /dev/sdb1
-umount /dev/sdb2
+sudo dd if=./sdimage-cyclone5.direct of=/dev/${prt}
+umount /dev/${prt}1
+umount /dev/${prt}2
+cd $OLDPWD
 echo -e "${yellow}SD card is ready!${NC}"
